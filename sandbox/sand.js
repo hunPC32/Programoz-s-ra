30074e897cc06e37e3bf961c3f180d298b4c5392
hunPC32 <hunpc7@gmail.com>
2023-01-24 16:19:35 +0100

    mind

--- sandbox/sand.js
+++ sandbox/sand.js
@@ -1,6 +1,21 @@
 console.log("hello")
 
-let a = 6
-let b = "12"
+let a = 100
+let b = "100"
+
+let amIHungry=true
+
+console.log(a)
+console.log(b)
 
 console.log(typeof(b))
+console.log(typeof(a))
+
+console.log(a-b)
+console.log(a+b)
+
+console.log(amIHungry)
+console.log(typeof(amIHungry))
+
+console.log(a===b)
+console.log(a==b)
