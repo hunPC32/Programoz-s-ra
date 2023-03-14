5f8d4356dea44b5bf1d964794d8550aceecf1c68
hunPC32 <hunpc7@gmail.com>
2023-03-14 11:06:45 +0100

    isdfhui

--- kopapir1/rps.js
+++ kopapir1/rps.js
@@ -9,11 +9,15 @@ options.push({'name':'Rock', 'emoji':'&#9994;'})
 options.push({'name':'Paper', 'emoji':'&#9995;'})
 options.push({'name':'Scissors', 'emoji':'&#9996;'})
 
+let playerHand=0
+
 
 
 for(let i = 0; i < choiceButtons.length; i++){
     choiceButtons[i].onclick = ()=>{
-        alert(choiceButtons[i].id)
+        reset()
+        playerHandEmoji=choiceButtons[i].innerHTML
+        go()
     }
     choiceButtons[i].innerHTML=options[i].emoji
 }
@@ -30,16 +34,28 @@ reset()
 function go(){
     
     let mod = -1
-    setInterval(() => {
+    let count = 0
+    let goInterval = setInterval(() => {
         enemyChoice.style.transform='rotate('+10*mod+'deg)'
 
         playerChoice.style.transform='rotateY(180deg) rotate('-90+10*mod+'deg)'
 
 
         mod=mod*-1
+        count+=1
+        if(count==7){
+            clearInterval(goInterval)
+            showHands()
+        }
     }, 200);
     
     
 }
 
-go()
+
+function showHands(){
+    let enemyChoiceRnd = Math.floor(Math.random()*options.length)
+    enemyChoice.innerHTML=options[enemyChoiceRnd].emoji
+
+    playerChoice.innerHTML=playerHandEmoji
+}
