a24660e2573d3963faa7556f1ebd447922af295a
hunPC32 <hunpc7@gmail.com>
2024-01-31 20:42:02 +0100

    Márk<3

---
+++ Márkpy/name.py
@@ -0,0 +1,21 @@
+dictionary = {
+    "name": "Márk",
+    "age": 16,
+    "school": True
+}
+name = "Márk"
+age = 16
+school = True
+list = [16 , 24 , 32]
+print(dictionary)
+#Norbi = "buzi"
+# + - * / // ** % 
+szam1 = int(input("Boldogsag: "))
+szam2 = int(input("Boldogsag1: "))
+print(szam1 + szam2)
+print(szam1 - szam2)
+print(szam1 * szam2)
+print(szam1 / szam2)
+print(szam1 // szam2)
+print(szam1 ** szam2)
+print(szam1 % szam2)
